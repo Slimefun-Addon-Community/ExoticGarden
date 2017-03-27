8db0fa79218d1d4e9aa47ac5a710c72090726dea
Goldentoenail <Goldentoenail@outlook.com>
2017-03-27 11:57:21 +0100

    Add files via upload

--- src/me/mrCookieSlime/ExoticGarden/EGPlant.java
+++ src/me/mrCookieSlime/ExoticGarden/EGPlant.java
@@ -5,9 +5,9 @@ import org.bukkit.inventory.ItemStack;
 
 import me.mrCookieSlime.Slimefun.Lists.RecipeType;
 import me.mrCookieSlime.Slimefun.Objects.Category;
-import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
+import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.HandledBlock;
 
-public class EGPlant extends SlimefunItem {
+public class EGPlant extends HandledBlock {
 
 	boolean edible;
 	
