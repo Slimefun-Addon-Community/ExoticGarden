161d7f0e766b771a5b2226cbd9c251848d9f310a
yurinogueira <yurinogueira@id.uff.br>
2020-05-07 07:29:22 -0300

    Unused import sorry

--- src/main/java/io/github/thebusybiscuit/exoticgarden/PlantsListener.java
+++ src/main/java/io/github/thebusybiscuit/exoticgarden/PlantsListener.java
@@ -4,7 +4,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ThreadLocalRandom;
 
 import io.papermc.lib.PaperLib;
