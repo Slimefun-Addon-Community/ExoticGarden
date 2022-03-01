8fae69a379040cab97576d9f6722df06d3e5c47c
Sleepyflea <99618885+Sleepyflea@users.noreply.github.com>
2022-03-01 14:24:36 -0600

    Update PlantsListener.java

--- src/main/java/io/github/thebusybiscuit/exoticgarden/listeners/PlantsListener.java
+++ src/main/java/io/github/thebusybiscuit/exoticgarden/listeners/PlantsListener.java
@@ -353,11 +353,41 @@ public class PlantsListener implements Listener {
         }
     }
 
+    Material[] blockedMaterials = new Material[] {
+    Material.ANVIL,
+    Material.CHIPPED_ANVIL,
+    Material.DAMAGED_ANVIL,
+    Material.DRAGON_EGG,
+    Material.SAND,
+    Material.RED_SAND,
+    Material.GRAVEL,
+    Material.SCAFFOLDING,
+    Material.WHITE_CONCRETE_POWDER,
+    Material.ORANGE_CONCRETE_POWDER,
+    Material.MAGENTA_CONCRETE_POWDER,
+    Material.LIGHT_BLUE_CONCRETE_POWDER,
+    Material.YELLOW_CONCRETE_POWDER,
+    Material.LIME_CONCRETE_POWDER,
+    Material.PINK_CONCRETE_POWDER,
+    Material.GRAY_CONCRETE_POWDER,
+    Material.LIGHT_GRAY_CONCRETE_POWDER,
+    Material.CYAN_CONCRETE_POWDER,
+    Material.PURPLE_CONCRETE_POWDER,
+    Material.BLUE_CONCRETE_POWDER,
+    Material.BROWN_CONCRETE_POWDER,
+    Material.GREEN_CONCRETE_POWDER,
+    Material.RED_CONCRETE_POWDER,
+    Material.BLACK_CONCRETE_POWDER,
+    };
+    
     @EventHandler
     public void onInteract(PlayerInteractEvent e) {
         if (e.getAction() != Action.RIGHT_CLICK_BLOCK) return;
         if (e.getHand() != EquipmentSlot.HAND) return;
         if (e.getPlayer().isSneaking()) return;
+        for(Material mat : blockedMaterials) {
+        if(mat == e.getPlayer().getInventory().getItemInMainHand().getType() || mat == e.getPlayer().getInventory().getItemInOffHand().getType()) return;
+        }
 
         if (Slimefun.getProtectionManager().hasPermission(e.getPlayer(), e.getClickedBlock().getLocation(), Interaction.BREAK_BLOCK)) {
             ItemStack item = ExoticGarden.harvestPlant(e.getClickedBlock());
